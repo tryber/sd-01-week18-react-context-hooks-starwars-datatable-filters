595e3a9657cec926e17cdd35d9dadb3186ec31f6
richardwellerson <richardwellerson@gmail.com>
2020-01-30 18:10:20 -0300

    Context da API feita

--- src/context/StarWarsContext.js
+++ src/context/StarWarsContext.js
@@ -1,5 +1,24 @@
-import { createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import { PropTypes } from 'prop-types';
+import getStarWarsPlanets from '../services/index';
 
 const StarWarsContext = createContext();
 
-export default StarWarsContext;
+const StarWarsProvider = ({ children }) => {
+  const [APIresult, setAPIresult] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    getStarWarsPlanets().then((planets) => setAPIresult(planets));
+    setIsFetching(true);
+  }, []);
+
+  const context = { APIresult, isFetching };
+  return <StarWarsContext.Provider value={context}>{children}</StarWarsContext.Provider>;
+};
+
+export { StarWarsContext, StarWarsProvider as Provider };
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
