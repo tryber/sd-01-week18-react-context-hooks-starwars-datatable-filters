5e5edce1cacaca71ec1e413016c59cd2eb7c5a7d
richardwellerson <richardwellerson@gmail.com>
2020-02-03 09:21:08 -0300

    Base da Tabela criada

--- src/components/Table.js
+++ src/components/Table.js
@@ -28,4 +28,22 @@ const BaseTable = () => {
   );
 };
 
+const FullTable = (planet) => (
+  <tr key={planet.name}>
+    <td>{`${planet.name}`}</td>
+    <td>{planet.orbital_period}</td>
+    <td>{planet.diameter}</td>
+    <td>{planet.climate}</td>
+    <td>{planet.gravity}</td>
+    <td>{planet.terrain}</td>
+    <td>{planet.surface_water}</td>
+    <td>{planet.population}</td>
+    <td>{/*planet.films*/}</td>
+    <td>{planet.created}</td>
+    <td>{planet.edited}</td>
+    <td>{planet.url}</td>
+
+  </tr>
+);
+
 export default BaseTable;
