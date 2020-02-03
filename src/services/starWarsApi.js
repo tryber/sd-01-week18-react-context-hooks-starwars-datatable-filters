a73706b16fff2b8b1ec253b555a4873f5f294965
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-03 14:41:23 -0300

    CC

--- src/services/starWarsApi.js
+++ src/services/starWarsApi.js
@@ -3,11 +3,6 @@ const STARWARS_API = 'https://swapi.co/api/planets/';
 const getStarWarsPlanets = () => (
   fetch(`${STARWARS_API}`)
     .then((response) => (
-      response.json()
-        .then((json) => ((response && Promise.resolve(json)))
-        )
-    )
-    )
-);
+      response.json().then((json) => response && Promise.resolve(json)))));
 
 export default getStarWarsPlanets;
