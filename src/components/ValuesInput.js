5b13ec092c6b2165ed070ce4becd9f52d3c8b637
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-04 16:28:46 -0300

    refatorando componente ValuesInput utilizando hooks

--- src/components/ValuesInput.js
+++ src/components/ValuesInput.js
@@ -1,109 +1,91 @@
-/* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import storeContext from '../context';
 
-class ValuesInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      column: '',
-      comparison: '',
-      value: '',
-    };
-  }
+function ValuesInput() {
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('Maior');
+  const [value, setValue] = useState('');
+  const {
+    setValuesFilter,
+    valuesFilter: { filters },
+  } = useContext(storeContext);
 
-  arrayOfColumns() {
-    const {
-      valuesFilter: { filters },
-    } = this.context;
-    const completeColumns = [
-      '',
-      'population',
-      'orbital_period',
-      'diameter',
-      'rotation_period',
-      'surface_water',
-    ];
+  const completeColumns = [
+    '',
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
+
+  function arrayOfColumns() {
     if (filters.length > 0) {
       const arrayOfUsedColumns = filters.map((filter) => filter.column);
       const arrayOfColumnsToUse = completeColumns.filter(
-        (column) => !arrayOfUsedColumns.includes(column),
+        (colum) => !arrayOfUsedColumns.includes(colum),
       );
       return arrayOfColumnsToUse;
     }
     return completeColumns;
   }
 
-  generateColumnOptions() {
-    return this.arrayOfColumns().map((each) => (
+  function generateColumnOptions() {
+    return arrayOfColumns().map((each) => (
       <option key={each} value={each}>
         {each}
       </option>
     ));
   }
 
-  updateStore(state) {
-    const { column, comparison, value } = state;
+  function updateStore() {
     const numericValues = { column, comparison, value };
-    const filters = [...this.context.valuesFilter.filters, state];
+    const newFilters = [...filters, { column, comparison, value }];
     if (column === '' || comparison === '' || value === '') {
       return alert('dados não preenchidos');
     }
-    this.setState({ column: '', comparison: '', value: '' });
-    return this.context.setValuesFilter({ numericValues, filters });
+    return setValuesFilter({ numericValues, filters: newFilters });
   }
 
-  changeState(event, id) {
-    this.setState({
-      [id]: event.target.value,
-    });
-  }
-
-  generateValuesInput() {
+  function generateValuesInput() {
     return (
       <div>
         <label htmlFor="column">
-          <select onChange={(e) => this.changeState(e, 'column')} data-testid="column" id="column">
-            {this.generateColumnOptions()}
+          <select onChange={(e) => setColumn(e, 'column')} data-testid="column" id="column">
+            {generateColumnOptions()}
           </select>
         </label>
         <select
-          onChange={(e) => this.changeState(e, 'comparison')}
+          onChange={(e) => setComparison(e, 'comparison')}
           data-testid="comparison"
           id="comparison"
         >
-          <option value="" />
           <option value="Maior">Maior que</option>
           <option value="Menor">Menor que</option>
           <option value="Igual">Igual</option>
         </select>
         <input
-          onChange={(e) => this.changeState(e, 'value')}
+          onChange={(e) => setValue(e, 'value')}
           data-testid="comparisonValue"
           id="comparisonValue"
           type="number"
           placeholder="Valor"
         />
-        <button type="button" onClick={() => this.updateStore(this.state)}>
+        <button type="button" onClick={updateStore}>
           Adicionar filtro
         </button>
       </div>
     );
   }
-
-  render() {
-    if (this.context.valuesFilter.filters.length === 5) {
-      return 'All filters are being used';
-    }
-    return (
-      <div>
-        Choose the column to filter:
-        <div>{this.generateValuesInput()}</div>
-      </div>
-    );
+  if (filters.length === 5) {
+    return 'All filters are being used';
   }
+  return (
+    <div>
+      Choose the column to filter:
+      <div>{generateValuesInput()}</div>
+    </div>
+  );
 }
-
-ValuesInput.contextType = storeContext;
 
 export default ValuesInput;
