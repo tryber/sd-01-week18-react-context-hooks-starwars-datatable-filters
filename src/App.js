c2e068832dd15e82bc8466b3647045aa31dffd80
richardwellerson <richardwellerson@gmail.com>
2020-02-04 19:02:18 -0300

    Code Climate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import BaseTable from './components/Table';
-import {Provider} from './context/StarWarsContext';
+import { Provider } from './context/StarWarsContext';
 import './App.css';
 
 function App() {
