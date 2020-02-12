213b7ae108f6a28630026bdd9c6abfe1e449c093
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-02-12 15:06:56 -0300

    depois eu faço

--- src/service/Comparisons.js
+++ src/service/Comparisons.js
@@ -22,21 +22,3 @@ export const columns = [
   'rotation_period',
   'surface_water',
 ];
-
-export const findComparisons = (data, filter) => {
-  const { column, comparison, value } = filter;
-  switch (comparison) {
-    case 'bigger than':
-      return data.filter(
-        (planet) => planet[column] > Number(value) && planet[column] !== 'unknown',
-      );
-    case 'less than':
-      return data.filter(
-        (planet) => planet[column] < Number(value) && planet[column] !== 'unknown',
-      );
-    case 'equal to':
-      return data.filter((planet) => planet[column] === value && planet[column] !== 'unknown');
-    default:
-      return false;
-  }
-};
