2aa86cf9714dcd4f4b7514ee72fdd4a261698488
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-15 12:05:38 -0200

    Louvre

--- src/components/DisplayFilterNum.jsx
+++ src/components/DisplayFilterNum.jsx
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import RemoveButton from './RemoveButton';
 import { comparisonSing } from '../services';
-
-
 
 const DisplayFilterNum = ({ filter, numericFilter, setNumericFilter }) => {
   const { column, value, comparison } = filter;
   return (
     <div style={{ display: 'flex' }}>
-      <p>
-        {column} |
-            {comparisonSing(comparison)} |
-            {value}
-      </p>
-      <RemoveButton column={column} numericFilter={numericFilter} setNumericFilter={setNumericFilter}/>
+      <p>{column} | {comparisonSing(comparison)} | {value}</p>
+      <RemoveButton
+        column={column}
+        numericFilter={numericFilter}
+        setNumericFilter={setNumericFilter}
+      />
     </div>
   );
-}
+};
 
-// DisplayFilterNum.propTypes = {
-//   addFilter: PropTypes.arrayOf(PropTypes.shape({
-//     value: PropTypes.string.isRequired,
-//   })),
-//   dispatchSomething: PropTypes.func.isRequired,
-//   availableCategories: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-// };
-
-// DisplayFilterNum.defaultProps = {
-//   addFilter: [],
-// };
+DisplayFilterNum.propTypes = {
+  filter: PropTypes.shape({
+    column: PropTypes.string.isRequired,
+    comparison: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+  }).isRequired,
+  numericFilter: PropTypes.objectOf(PropTypes.object),
+  setNumericFilter: PropTypes.func.isRequired,
+};
 
 export default DisplayFilterNum;
