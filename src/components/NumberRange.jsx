a1de95e1a3c189fe9de87a17f0c889b6eadab42f
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-15 12:36:57 -0200

    Number Range

--- src/components/NumberRange.jsx
+++ src/components/NumberRange.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const NumberRange = ({ numericFilter, setNumericFilter }) => {
-  return (
-    <div>
-      <input
-        type="number"
-        onChange={(e) => setNumericFilter({ ...numericFilter, value: e.target.value })}
-      />
-    </div>
-  );
-}
+const NumberRange = ({ numericFilter, setNumericFilter }) => (
+  <input
+    type="number"
+    onChange={(e) => setNumericFilter({ ...numericFilter, value: e.target.value })}
+  />
+);
 
-// NumberRange.propTypes = {
-//   dispatchSomething: PropTypes.func.isRequired,
-// };
+NumberRange.propTypes = {
+  numericFilter: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+    })).isRequired,
+  setNumericFilter: PropTypes.func.isRequired,
+};
 
 export default NumberRange;
