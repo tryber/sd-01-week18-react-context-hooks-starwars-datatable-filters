a7cd51505039428d23ba6492aeea122be5503a84
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-02-17 16:05:59 -0300

    shearch bar text

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,7 +8,7 @@ afterEach(cleanup);
 
 services.fetchingPlanets = jest.fn(() =>
   new Promise((resolve) => setTimeout(() => resolve(planetsData), 3000)));
-  
+
 describe('async', () => {
   test('testing receive data by categories', async () => {
     const { getByTestId } = render(<App />);
@@ -30,16 +30,52 @@ describe('async', () => {
     }
   });
 
+  const filterPlanets = (string) => (
+    planetsData.results
+      .filter(({ name }) => name.toLowerCase().includes(string.toLowerCase()))
+      .map(({ name }) => name)
+  );
+
   test('shearch bar text', async () => {
-    const { debug, getByTestId } = render(<App />);
+    const { getByTestId } = render(<App />);
 
     await waitForDomChange();
-    
-    fireEvent.change(getByTestId('search-bar'), { target: { value: 'mother' } });
 
-    const filterPlanets = planetsData.results.filter(({ name }) => name.includes('mother'));
-    console.log(filterPlanets)
-    // debug()
+    fireEvent.change(getByTestId('search-bar'), { target: { value: 'moTher' } });
+
+    const filterPlanetsI = filterPlanets('moTher');
+    const renderPlanetsI = [];
+    for (let i = 0; i < filterPlanetsI.length; i += 1) {
+      renderPlanetsI.push(getByTestId(`rowname${i}`).innerHTML)
+    };
+    expect(renderPlanetsI.length).toBe(filterPlanetsI.length);
+    for (let i = 0; i < filterPlanetsI.length; i += 1) {
+      expect(filterPlanetsI.includes(renderPlanetsI[i])).toBeTruthy()
+    };
+
+    fireEvent.change(getByTestId('search-bar'), { target: { value: '' } });
+
+    const filterPlanetsII = planetsData.results.map(({ name }) => name);
+    const renderPlanetsII = [];
+    for (let i = 0; i < filterPlanetsII.length; i += 1) {
+      renderPlanetsII.push(getByTestId(`rowname${i}`).innerHTML)
+    };
+    expect(renderPlanetsII.length).toBe(filterPlanetsII.length);
+    for (let i = 0; i < filterPlanetsII.length; i += 1) {
+      expect(filterPlanetsII.includes(renderPlanetsII[i])).toBeTruthy()
+    };
+
+    fireEvent.change(getByTestId('search-bar'), { target: { value: 'aurora' } });
+
+    const filterPlanetsIII = filterPlanets('aurora');
+    const renderPlanetsIII = [];
+    for (let i = 0; i < filterPlanetsIII.length; i += 1) {
+      renderPlanetsIII.push(getByTestId(`rowname${i}`).innerHTML)
+    };
+    expect(renderPlanetsIII.length).toBe(filterPlanetsIII.length);
+    for (let i = 0; i < filterPlanetsIII.length; i += 1) {
+      expect(filterPlanetsIII.includes(renderPlanetsIII[i])).toBeTruthy()
+    };
   });
 
   test('loading testing', () => {
@@ -47,7 +83,7 @@ describe('async', () => {
     expect(getByText('Loading...')).toBeInTheDocument();
   });
 
-  
+
 })
 
 
